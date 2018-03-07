681e73312f56aaec7e00fe3d9c2082583111f885
user1 <user1@DESKTOP-BIBKLL9>
2018-03-07 20:07:49 +0900

    0307 20:07

--- src/main/java/spring/day0307/GreetingTest.java
+++ src/main/java/spring/day0307/GreetingTest.java
@@ -2,11 +2,12 @@ package spring.day0307;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class GreetingTest {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("aopTest.xml");
+		ApplicationContext ctx = new GenericXmlApplicationContext("aopTest.xml");
 		GreetingService bean = ctx.getBean("greetingTarget", GreetingService.class);
 
 		try {
