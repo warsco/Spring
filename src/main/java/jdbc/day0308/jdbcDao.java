2b33eac4d2b2b24a5c9fe1b922b89bfe412b2ab7
user1 <user1@DESKTOP-BIBKLL9>
2018-03-09 10:18:47 +0900

    0309-10:18

--- src/main/java/jdbc/day0308/jdbcDao.java
+++ src/main/java/jdbc/day0308/jdbcDao.java
@@ -3,24 +3,25 @@ package jdbc.day0308;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
-public class jdbcDao {
-	JdbcTemplate jdbcTemplate;
+public class jdbcDao extends JdbcDaoSupport{
 	UserRowMapper mapper = new UserRowMapper();
-	
+	/*JdbcTemplate jdbcTemplate;
+
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
-	}
+	}*/
 	
 	public List<User> selectAll(){
 		String sql = "select*from member";
-		List<User> member = jdbcTemplate.query(sql, mapper);
+		List<User> member = getJdbcTemplate().query(sql, mapper);
 		return member;
 	}
 	
 	public void insert() {
 		String sql = "insert into member values('abc3', '123', '권우진3','이메일@이메일')";
-		jdbcTemplate.update(sql);
+		getJdbcTemplate().update(sql);
 	}
 	
 
